ee37eb99ed4897c9b15cf870dd4d3d84cba796ac
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-08-06 17:51:44 -0500

    solving Queue

--- queue.js
+++ queue.js
@@ -20,26 +20,46 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
   enqueue(val) {
+    const newQueue = new Node(val);
+    if (!this.first) {
+      this.first = newQueue;
+      this.last = newQueue;
+      this.size = 1;
 
+    } else {
+      const node = this.last;
+      node.next = newQueue;
+      this.last = newQueue;
+      this.size++;
+
+    }
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
   dequeue() {
-
+    if (this.size === 0) return "queue is empty";
+    else {
+      const removedQueue = this.first;
+      this.first = this.first.next;
+      this.size--;
+      return removedQueue;
+    }
   }
 
   /** peek(): return the value of the first node in the queue. */
 
   peek() {
-
+    if (this.size === 0) return "Queue is empty";
+    else return this.first;
   }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
   isEmpty() {
-
+    if (this.size === 0) return true;
+    else return false;
   }
 }
 
